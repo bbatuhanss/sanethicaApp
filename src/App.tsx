206db931e25ae5a075ce8d07e13a0dc91874f43d
Hasan Batuhan Sevinc <bbatuhansevincc@gmail.com>
2026-06-11 17:24:28 +0300

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import AOS from "aos";
 import MaintenancePage from "pages/maintenance/MaintenancePage";
 
